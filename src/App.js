70974030ae87ffdecf23bf8ba02040bc555ab3fb
umer <umer4167@hotmail.com>
2019-10-31 13:26:45 +0200

    card basic styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Cards from "./components/Cards";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
       <Cards />
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Cards.css";
 
 export default function Cards() {
   return (
@@ -7,6 +8,40 @@ export default function Cards() {
         <div className="face face1">
           <div className="content">
             <img src={require("../assets/design.png")} alt="" />
+            <h3>Design</h3>
+          </div>
+        </div>
+        <div className="face face2">
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+              aut.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="face face1">
+          <div className="content">
+            <img src={require("../assets/data.png")} alt="" />
+            <h3>Design</h3>
+          </div>
+        </div>
+        <div className="face face2">
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+              aut.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="face face1">
+          <div className="content">
+            <img src={require("../assets/startup.png")} alt="" />
             <h3>Design</h3>
           </div>
         </div>
