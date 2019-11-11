680ae83b9a8f3e7a1b7377863a795c918131b15c
umer <umer4167@hotmail.com>
2019-11-11 20:03:39 +0200

    fixing classname and markup

--- src/components/Darkmode.js
+++ src/components/Darkmode.js
@@ -2,8 +2,23 @@ import React from "react";
 import "./Darkmode.scss";
 export default function Darkmode() {
   return (
-    <div>
+    <>
       <h1>darkmode</h1>
-    </div>
+      <section className="intro">
+        <h1>Amazing Product</h1>
+        <p className="subtitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          rem molestias ipsum nulla libero optio.
+        </p>
+        <div className="btn-group">
+          <a href="#" className="btn btn-accent">
+            Purchase
+          </a>
+          <a href="#" className="btn btn-secondary">
+            More info
+          </a>
+        </div>
+      </section>
+    </>
   );
 }
